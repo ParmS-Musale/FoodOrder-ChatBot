26cd430a2c170d6227d269d2c5c2412e47bf38a2
ParmS-Musale <musaleparm9541@gmail.com>
2024-07-21 19:31:54 +0530

    All Set..!!

---
+++ Bot/bot.js
@@ -0,0 +1,18 @@
+// const { Telegraf } = require("telegraf");
+// const TOKEN = "7422644875:AAF_mGBzHE8FsxGL0Exxn7gO3mHXe1zeI5g";
+// const bot = new Telegraf(TOKEN);
+
+// const web_link = "https://foodorder-chatbot.netlify.app/";
+
+// bot.start((ctx) =>
+//   ctx.reply("Welcome :)))))", {
+//     reply_markup: {
+//       keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+//     },
+//   })
+// );
+
+// bot.launch();
+
+
+
